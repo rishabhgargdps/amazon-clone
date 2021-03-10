afa38a07c439d9416f4d959f06bb5f9a80515f14
rishabhgarg <rishabhgargdps@gmail.com>
2021-03-10 12:41:41 +0530

    Initial react and firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC1m0QGnlggKS5uDRV6WyXvVyEMZryPVek",
+  authDomain: "channel-64b8d.firebaseapp.com",
+  projectId: "channel-64b8d",
+  storageBucket: "channel-64b8d.appspot.com",
+  messagingSenderId: "346843458753",
+  appId: "1:346843458753:web:91ea0c50d29a91832ef840",
+};
