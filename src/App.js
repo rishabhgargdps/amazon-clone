4450d69cfe6c978df5775e481212d355229b1aba
rishabhgarg <rishabhgargdps@gmail.com>
2021-03-11 01:17:56 +0530

    Checkout page component initialized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
-import Home from "./Home"
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
